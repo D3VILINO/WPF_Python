ecdf93f8a2057d8f0687d5e2980aa8c39238cf8c
Danilo Görtiz <goritzd@gmail.com>
2025-02-24 08:44:36 +0100

    wpf aufgaben 1 und teil von 2

---
+++ Dino/woche05/Uebungsaufgaben_28.01.2025.py
@@ -0,0 +1,69 @@
+class Stack:
+  def __init__(self):
+    self.st = []
+
+  def push(self, value):
+    self.st.append(value)
+
+  def pop(self):
+    return self.st.pop()
+
+  def __len__(self):
+    return len(self.st)
+
+  def toList(self):
+    return list(self.st)
+
+  def multPop(self, n:int):
+    popped = list()
+    for i in range(n):
+      popped.append(self.pop())
+    return popped
+
+s = Stack()
+s.push(1); s.push("Hallo"); s.push(4.32); s.push(True)
+print(s.pop())
+print("--------")
+s.push("a")
+print(s.pop(), s.pop())
+print(s.pop())
+print("--------")
+s.push(1); s.push("Hallo"); s.push(4.32); s.push(True)
+print(s)
+print("--------")
+print(s.toList())
+print("--------")
+print(s.multPop(3))
+print("--------")
+print(s.__len__())
+
+# ----------------------------------------------------------------
+print("\n\nAufgabe 2")
+class AufgabenManager:
+  def __init__(self):
+    self.aufgaben = {}
+
+  def neueAufgabe(self, aufgabe:str, prioritaet:int):
+    self.aufgaben[aufgabe] = prioritaet
+
+  def hoechstePrio(self):
+    minPrio = 9999999999999
+    for prioritaet in self.aufgaben:
+      if self.aufgaben[prioritaet] < minPrio:
+        minPrio = self.aufgaben[prioritaet]
+    return minPrio
+
+  def erledigeNaechsteAufgabe(self):
+    pass
+
+aufgs = AufgabenManager()
+aufgs.neueAufgabe("Kueche putzen", 5)
+aufgs.neueAufgabe("Auf Prog 1 lernen", 1)
+aufgs.neueAufgabe("Oma besuchen", 2)
+aufgs.neueAufgabe("Auf Mathe 1 lernen", 1)
+aufgs.neueAufgabe("Fahrrad putzen", 10)
+# aufgs.erledigeNaechsteAufgabe ()
+# aufgs.erledigeNaechsteAufgabe ()
+print(aufgs.hoechstePrio())
+# print(aufgs.anzahlAufgabenPrio())
+# print(anzahlAufgaben())
