69b3fb9200daa41ae79e275bb5508e88ea441892
Danilo <goritzd@gmail.com>
2025-03-28 12:26:59 +0100

    funktionsloses einfügen und löschen

--- Dino/woche07/taschenrechner/contents.py
+++ Dino/woche07/taschenrechner/contents.py
@@ -1,4 +1,7 @@
-from tkinter import Button, Entry, Frame
+from functools import partial
+from tkinter import END, Button, Entry, Frame, Label
+
+label_display:Label
 
 def create_calculator(master:Frame) -> None:
   _create_display(master)
@@ -6,8 +9,11 @@ def create_calculator(master:Frame) -> None:
   _create_body(master)
 
 def _create_display(master:Frame) -> None:
-  label_display = Entry(master, state="disabled", width=30)
-  label_display.pack(pady=10)
+  global label_display
+  frame_display = Frame(master, bg="black")
+  frame_display.pack()
+  label_display = Label(frame_display, width=30, bg="white")
+  label_display.pack(padx=1, pady=1)
 
 def _create_left_collumn(master:Frame) -> None:
   frame_left_column = Frame(master)
@@ -18,7 +24,7 @@ def _create_left_collumn(master:Frame) -> None:
 
   buttons_memory = ("MC", "MR", "MS", "M+")
   for text in buttons_memory:
-    button = Button(frame_left_column, text=text, borderwidth=5)
+    button = Button(frame_left_column, text=text, borderwidth=5, command=partial(_add_to_display, text))
     button.pack(pady="1")
 
 def _create_body(master:Frame) -> None:
@@ -28,7 +34,7 @@ def _create_body(master:Frame) -> None:
   # Comands
   top_row = (("Backspace", 0), ("CE", 5), ("C", 10))
   for text, column in top_row:
-    button = Button(frame_body, text=text, width=8, borderwidth=5)
+    button = Button(frame_body, text=text, width=8, borderwidth=5, command=partial(_delete_from_display, text))
     button.grid(row=0, column=column, columnspan=5, padx=1, pady=5, sticky="ew")
 
   # Inputs
@@ -36,22 +42,38 @@ def _create_body(master:Frame) -> None:
   frame_inputs.grid(row=1, column=0, columnspan=15, rowspan=3, sticky="ew")
   
   for i in range(0,9):
-    button = Button(frame_inputs, text=f"{i+1}", borderwidth=5, command=_add_to_display)
+    button = Button(frame_inputs, text=f"{i+1}", borderwidth=5, command=partial(_add_to_display, str(i+1)))
     button.grid(row=2-(i//3), column=i%3, padx=1, pady=1, sticky="ew")
   
   bottom_row = (("0", 0), ("+/-", 1), (".", 2))
   for text, column in bottom_row:
-    button = Button(frame_inputs, text=text, borderwidth=5, command=_add_to_display)
+    button = Button(frame_inputs, text=text, borderwidth=5, command=partial(_add_to_display, text))
     button.grid(row=3, column=column, padx=1, pady=1, sticky="ew")
   
   right_columns = (("/", 0, 3), ("Sqrt", 0, 4), ("*", 1, 3), ("%", 1, 4), ("-", 2, 3), ("1/x", 2, 4), ("+", 3, 3), ("=", 3, 4))
   for text, row, column in right_columns:
-    button = Button(frame_inputs, text=text, borderwidth=5, command=_add_to_display)
+    button = Button(frame_inputs, text=text, borderwidth=5, command=partial(_add_to_display, text))
     button.grid(row=row, column=column, padx=1, pady=1, sticky="ew")
 
   for i in range(5):
     frame_inputs.columnconfigure(i, weight=1)
 
-def _add_to_display():
-  pass
+def _add_to_display(s:str) -> None:
+  global label_display
+  label_display.config(text=label_display.cget("text")+s)
   
+def _delete_from_display(s:str) -> None:
+  global label_display
+  match s:
+    case "C":
+      label_display.config(text="")
+    case "CE":
+      label_display.config(text="") # noch keine funktion zwischenrechnungen zu speichern
+    case "Backspace":
+      text = label_display.cget("text")
+      if text == "":
+        return
+      if text[-1] == "t":
+        label_display.config(text=text[0:-4])
+      else: 
+        label_display.config(text=text[0:-1])
