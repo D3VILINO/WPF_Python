fc81a237d1e9417aac50ddb4f73f968ba3e4da88
Danilo <goritzd@gmail.com>
2025-03-28 11:37:56 +0100

    Struktur fertiggestellt

--- Dino/woche07/taschenrechner/contents.py
+++ Dino/woche07/taschenrechner/contents.py
@@ -1,31 +1,57 @@
 from tkinter import Button, Entry, Frame
 
-def create_display(master:Frame) -> None:
+def create_calculator(master:Frame) -> None:
+  _create_display(master)
+  _create_left_collumn(master)
+  _create_body(master)
+
+def _create_display(master:Frame) -> None:
   label_display = Entry(master, state="disabled", width=30)
-  label_display.pack()
+  label_display.pack(pady=10)
 
-def create_left_collumn(master:Frame) -> None:
-  label_what = Entry(master, state="disabled", font=("Helvetica", 16), width=2, border=1, borderwidth=1)
-  label_what.pack(pady="1")
+def _create_left_collumn(master:Frame) -> None:
+  frame_left_column = Frame(master)
+  frame_left_column.pack(side="left")
 
-  button_MC = Button(master, text="MC")
-  button_MC.pack(pady="1")
+  label_what = Entry(frame_left_column, state="disabled", font=("Helvetica", 16), width=2)
+  label_what.pack(pady="7")
 
-  button_MR = Button(master, text="MR")
-  button_MR.pack(pady="1")
+  buttons_memory = ("MC", "MR", "MS", "M+")
+  for text in buttons_memory:
+    button = Button(frame_left_column, text=text, borderwidth=5)
+    button.pack(pady="1")
 
-  button_MS = Button(master, text="MS")
-  button_MS.pack(pady="1")
+def _create_body(master:Frame) -> None:
+  frame_body = Frame(master)
+  frame_body.pack()
 
-  button_M_add = Button(master, text="M+")
-  button_M_add.pack(pady="1")
+  # Comands
+  top_row = (("Backspace", 0), ("CE", 5), ("C", 10))
+  for text, column in top_row:
+    button = Button(frame_body, text=text, width=8, borderwidth=5)
+    button.grid(row=0, column=column, columnspan=5, padx=1, pady=5, sticky="ew")
 
-def create_body(master:Frame) -> None:
-  button_backspace = Button(text="Backspace")
-  button_backspace.grid(row=0, column=0, columnspan=5, padx=1, pady=1)
+  # Inputs
+  frame_inputs = Frame(frame_body)
+  frame_inputs.grid(row=1, column=0, columnspan=15, rowspan=3, sticky="ew")
+  
+  for i in range(0,9):
+    button = Button(frame_inputs, text=f"{i+1}", borderwidth=5, command=_add_to_display)
+    button.grid(row=2-(i//3), column=i%3, padx=1, pady=1, sticky="ew")
+  
+  bottom_row = (("0", 0), ("+/-", 1), (".", 2))
+  for text, column in bottom_row:
+    button = Button(frame_inputs, text=text, borderwidth=5, command=_add_to_display)
+    button.grid(row=3, column=column, padx=1, pady=1, sticky="ew")
+  
+  right_columns = (("/", 0, 3), ("Sqrt", 0, 4), ("*", 1, 3), ("%", 1, 4), ("-", 2, 3), ("1/x", 2, 4), ("+", 3, 3), ("=", 3, 4))
+  for text, row, column in right_columns:
+    button = Button(frame_inputs, text=text, borderwidth=5, command=_add_to_display)
+    button.grid(row=row, column=column, padx=1, pady=1, sticky="ew")
 
-  button_CE = Button(text="CE")
-  button_CE.grid(row=0, column=5, columnspan=5, padx=1, pady=1)
+  for i in range(5):
+    frame_inputs.columnconfigure(i, weight=1)
 
-  button_C = Button(text="C")
-  button_C.grid(row=0, column=10, columnspan=5, padx=1, pady=1)
+def _add_to_display():
+  pass
+  
--- Dino/woche07/taschenrechner/main.py
+++ Dino/woche07/taschenrechner/main.py
@@ -1,18 +1,16 @@
 import os
-from tkinter import Button, Frame, PhotoImage, Tk
-import contents
+from tkinter import Frame, PhotoImage, Tk
+from contents import create_calculator
 
 window = Tk()
 window.title("Calculator")
 window.iconphoto(False, PhotoImage(file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "icon.png")))
 window.resizable(False, False)
-window.geometry("200x200")
+window.geometry("315x250")
 
 root = Frame(window)
-root.pack(anchor="center", fill="both", pady="10", padx="10")
+root.pack(anchor="center", fill="both", pady="10", padx="25")
 
-contents.create_display(root)
-contents.create_left_collumn(root)
-contents.create_body(root)
+create_calculator(root)
 
 window.mainloop()
