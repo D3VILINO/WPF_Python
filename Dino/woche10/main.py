50b4c63d7017a9f3046597888b4d8e2195fd2e08
Danilo Görtiz <goritzd@gmail.com>
2025-06-12 15:59:01 +0200

    langeweile schöne farbe

--- Dino/woche10/main.py
+++ Dino/woche10/main.py
@@ -1,3 +1,7 @@
 from visualisation import open_window
 
-open_window()
+try:
+  open_window()
+except BaseException as e:
+  print(f"\033[34mFehler: {type(e).__name__}{(": " + str(e)) if str(e) else ""}\033[0m") # Blau weil ich das ganze Rot so langsam nicht mehr sehen kann
+  exit(0)
